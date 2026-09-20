c382bf41322fe28f94bb124bf0e878ede2d1fa89
vvb333007 <vvb333007@gmail.com>
2026-09-20 18:49:32 +0700

    +	amalgamated version

---
+++ examples/linux_windows_example/amalgam.c
@@ -0,0 +1,22 @@
+/* Amalgamated source
+ *
+ */
+#define AMALGAM 1
+
+#include "../../src/ph7.h"
+#include "../../src/ph7int.h"
+#include "../../src/vm.c"
+#include "../../src/vfs.c"
+#include "../../src/posix_vfs.c"
+#include "../../src/parse.c"
+#include "../../src/oo.c"
+#include "../../src/memobj.c"
+#include "../../src/lib.c"
+#include "../../src/lex.c"
+#include "../../src/hashmap.c"
+#include "../../src/constant.c"
+#include "../../src/esp32/constants.c"
+#include "../../src/compile.c"
+#include "../../src/builtin.c"
+#include "../../src/esp32/builtins.c"
+#include "../../src/api.c"
